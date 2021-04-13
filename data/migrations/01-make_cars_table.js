1c43bfad260fe73529789ffa526df456fdd3e0b8
Will B <wberman27@gmail.com>
2021-04-13 18:41:49 -0400

    added cars schema to migrations

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,14 @@
-// DO YOUR MAGIC
+exports.up = function(knex){
+    return knex.schema.createTable("cars", table =>{
+        table.increments(); //create primary key (id), which auto increments
+        table.text("vin", 128).unique().notNullable();
+        table.text("make").notNullable();
+        table.text("model").notNullable();
+        table.decimal("mileage").notNullable();
+        table.text("title", 128)
+        table.text("transmission", 128)
+    })
+}
+exports.down = function(knex){
+    return knex.schema.dropTableIfExists("cars")
+}
